96b088ba4407cacce629c7dd3adc8cfe7516acd1
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-09-26 12:52:34 +0800

    Added assets

---
+++ hasuratraining_september_2022/scratch.sql
@@ -0,0 +1,10 @@
+insert into region_info (name, sales_director) values
+('NORTHEAST', 'Bill'),
+('MIDWEST', 'Ted'),
+('SOUTH', 'Hong'),
+('PLAINS', 'Yuki'),
+('APPALACHIA', 'Uduok'),
+('MOUNTAIN', 'Maria'),
+('NORTHWEST', 'Benoit'),
+('WEST', 'Harish'),
+('SOUTHWEST', 'Akhil');
