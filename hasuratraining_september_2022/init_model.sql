ef3808605bfbfe4afd6e94f49df37e7f9bc1e8df
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-09-27 05:32:56 +0800

    Added the account_summary view

--- hasuratraining_september_2022/init_model.sql
+++ hasuratraining_september_2022/init_model.sql
@@ -144,3 +144,15 @@ alter table "public"."order"
   ("value") on update restrict on delete restrict;
 
 create index on "order" (region);
+
+-- account summary view
+
+create or replace view account_summary as
+select
+  account.id,
+  sum(units*price)
+  from account
+       join "order" on "order".account_id = account.id
+       join order_detail on order_detail.order_id = "order".id
+       join product on product.id = order_detail.product_id
+ group by account.id;
