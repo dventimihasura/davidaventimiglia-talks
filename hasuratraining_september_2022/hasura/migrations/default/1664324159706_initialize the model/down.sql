953c71072e9c306da81bb364b162371f0f37eb82
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-09-28 08:20:30 +0800

    Initialized the data model

---
+++ hasuratraining_september_2022/hasura/migrations/default/1664324159706_initialize the model/down.sql
@@ -0,0 +1,15 @@
+drop table if exists account cascade;
+
+drop table if exists "order" cascade;
+
+drop table if exists order_detail cascade;
+
+drop table if exists product cascade;
+
+drop function if exists product_search cascade;
+
+drop function if exists product_search_slow cascade;
+
+drop view if exists account_summary cascade;
+
+drop function if exists product_sku cascade;
