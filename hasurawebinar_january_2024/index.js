2a7c11be0b963d6404d0426743ac0588352d5758
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-01-25 13:01:48 -0800

    Added the composed SDL

--- hasurawebinar_january_2024/index.js
+++ hasurawebinar_january_2024/index.js
@@ -1,16 +1,17 @@
 const { ApolloServer } = require('apollo-server');
 const { ApolloGateway } = require('@apollo/gateway');
+const { readFileSync } = require('fs');
 
-const supergraphSdl = ''; // TODO!
+const supergraphSdl = readFileSync('./supergraph.graphql').toString();
 
 const gateway = new ApolloGateway({
-    supergraphSdl
+  supergraphSdl
 });
 
 const server = new ApolloServer({
-    gateway,
+  gateway,
 });
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 Gateway ready at ${url}`);
+  console.log(`🚀 Gateway ready at ${url}`);
 }).catch(err => {console.error(err)});
